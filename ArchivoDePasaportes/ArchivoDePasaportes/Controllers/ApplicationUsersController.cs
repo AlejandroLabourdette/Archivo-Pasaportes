c515e987cfafb4db9b01949d8e1243696442750c
Nadia <Nadia@DESKTOP-9A9EADU>
2020-10-27 04:45:55 -0400

    Create views broken

---
+++ ArchivoDePasaportes/ArchivoDePasaportes/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArchivoDePasaportes.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArchivoDePasaportes.Controllers
+{
+    public class ApplicationUsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ApplicationUsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = from u in _context.Users select u;
+            var usersList = users.ToList();
+
+            return View(usersList);
+        }
+    }
+}
