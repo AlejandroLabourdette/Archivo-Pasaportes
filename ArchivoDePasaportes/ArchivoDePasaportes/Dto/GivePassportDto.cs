8b48332b6b8e0e4039cdace1d40b817cc369d1c3
ALabourdette <j.labourdette@estudiantes.matcom.uh.cu>
2020-12-09 15:06:08 -0800

    Give option in ListPassport

--- ArchivoDePasaportes/ArchivoDePasaportes/Dto/GivePassportDto.cs
+++ ArchivoDePasaportes/ArchivoDePasaportes/Dto/GivePassportDto.cs
@@ -9,10 +9,14 @@ namespace ArchivoDePasaportes.Dto
     public class GivePassportDto
     {
         [Required]
+        [Display(Name ="Número de Pasaporte")]
         public string PassportNo { get; set; }
         [Required]
+        [Display(Name ="Fecha de Entrega")]
         public DateTime GiveDate { get; set; }
+        [Display(Name ="Fecha de Devolución")]
         public DateTime? ExpectedReturn { get; set; }
+        [Display(Name ="Descripción")]
         public string Description { get; set; }
     }
 }
